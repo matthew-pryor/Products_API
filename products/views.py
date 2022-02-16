8ec10819a2285a849602f198cb9aff0ee8395c35
matthew-pryor <pryor.matthew.a@gmail.com>
2022-02-16 13:21:20 -0600

    created new view function for getting products by id + created delete function + created update function + added new URL path for car_detail

--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_list_or_404, get_object_or_404
 from .serializers import ProductSerializer
 from .models import Product
-from django.shortcuts import get_list_or_404, get_object_or_404
 from importlib.util import resolve_name
-from rest_framework import status
 from products import serializers
 
 
@@ -25,4 +25,20 @@ def products_list(request):
         serializer.save()
         return Response(serializer.data, status = status.HTTP_201_CREATED)
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
 
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
