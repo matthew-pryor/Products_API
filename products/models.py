d5e41270d7c75be331861aae307ce8d2e640a2e3
matthew-pryor <pryor.matthew.a@gmail.com>
2022-02-16 12:05:54 -0600

    Registered product model with admin site + completed GET all products funtionality for Product + completed POST functionality for Product + updated Rest framework decimal settings

--- products/models.py
+++ products/models.py
@@ -7,4 +7,4 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    inventory_quantitiy = models.IntegerField()
+    inventory_quantity = models.IntegerField()
--- products/views.py
+++ products/views.py
@@ -1,9 +1,28 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .models import Product
+from django.shortcuts import get_list_or_404, get_object_or_404
+from importlib.util import resolve_name
+from rest_framework import status
+from products import serializers
 
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def products_list(request):
 
-    return Response('ok')
+    if request.method == 'GET':
+
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+
